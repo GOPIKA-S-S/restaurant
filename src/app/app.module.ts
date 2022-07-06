fc9926c64382e6604c99b5103d3274743731825a
GOPIKA-S-S <gopikass1024@gmail.com>
2022-07-06 19:50:40 +0530

    component completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchRestaurantComponent } from './search-restaurant/search-restaurant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchRestaurantComponent
   ],
   imports: [
     BrowserModule,
